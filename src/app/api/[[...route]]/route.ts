8a62639cfa8dc41768bc80d19a62966a76a792e2
ericxu9 <xuyongjun96@gmail.com>
2025-04-28 22:46:19 +0800

    Add tanstack react query

---
+++ src/app/api/[[...route]]/route.ts
@@ -0,0 +1,23 @@
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+
+export const runtime = "edge";
+
+const app = new Hono().basePath("/api");
+
+app.get("/hello", (c) => {
+  return c.json({
+    message: "Hello Next.js!",
+  });
+});
+
+app.get("/user/:name", async (c) => {
+  const name = c.req.param("name");
+  // ...
+  return c.json({
+    name,
+  });
+});
+
+export const GET = handle(app);
+export const POST = handle(app);
